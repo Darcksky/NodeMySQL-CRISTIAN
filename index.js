ba7d00c7cc74069103035d10215c1bc636a16fc6
Cristian Andres Taborda Cuadrado <andrelogames052@gmail.com>
2020-11-29 13:38:21 +0000

    pusieron cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.json());
+app.use(cors())
 
 app.get('/', (req, res)=>{
   res.send('Esto posiblemente salga mal')
